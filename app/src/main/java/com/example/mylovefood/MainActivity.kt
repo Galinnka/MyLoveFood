7ce04d7832bd58689e2d77c60acdd1ebd9c21df6
Galina <118059223+Galinnka@users.noreply.github.com>
2024-03-15 02:25:01 +0300

    Recipes bottom navigation

---
+++ app/src/main/java/com/example/mylovefood/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.mylovefood
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.example.mylovefood.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+// для переключения цветов кнопок боттом меню использовала селектор в дравбл
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(RecipeFragment())
+
+        //запуск функции переключателя фрагментов в боттом навигации
+        binding.btmNav.setOnItemReselectedListener {
+            when(it.itemId) {
+                R.id.RecipeFragment -> replaceFragment(RecipeFragment())
+                R.id.FavoriteFragment -> replaceFragment(FavoritesFragment())
+                R.id.CategoryFragment -> replaceFragment(CategoryFragment())
+                else -> {
+
+                }
+            }
+            true
+        }
+
+
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.host_fragment, fragment)
+        fragmentTransaction.commit()
+    }
+}
